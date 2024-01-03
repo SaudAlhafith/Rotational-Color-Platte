2c60206622192f8f4df5e72bfd120440bbc6de1a
SaudAlhafith <SaudAlhafith@gmail.com>
2024-01-03 17:56:27 +0300

    Initial Commit

---
+++ RotationalColorPlatte/RotationalColorPlatteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotationalColorPlatteApp.swift
+//  RotationalColorPlatte
+//
+//  Created by saudAlhafith on 21/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct RotationalColorPlatteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
